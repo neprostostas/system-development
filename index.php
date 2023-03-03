a6daa6929b7af48ef70800be172f76a35e6925e7
neprostostas <neprostostas.gg@gmail.com>
2023-03-03 21:19:43 +0200

    added three-cards, tablet-block, blocks-square BLOCKS

--- index.php
+++ index.php
@@ -36,7 +36,6 @@
                 </div>
 
             </div>
-
             <div class="block-developers">
                 <div class="left-part">
                     <div class="green-block"></div>
@@ -78,6 +77,125 @@
                         <img src="style/icons/line-8.svg" alt="line-8">
                         <p>Забудовники</p>
                     </div>
+                </div>
+            </div>
+            <div class="three-cards">
+                <div class="card">
+                    <h3>Ми беремо лише 1 клієнта на 1 місто України</h3>
+                    <p>щоб бути в фокусі, викладатися на 100% і надати максимально можливий результат саме для вас</p>
+                </div>
+                <div class="card">
+                    <h3>Замість 5 підрядників Ви наймаєте одного,</h3>
+                    <p>який за рахунок свого досвіду в ніші повністю виконує весь спектр онлайн-робіт Вашого бізнесу під ключ!</p>
+                </div>
+                <div class="card">
+                    <h3>Замість 5 підрядників Ви наймаєте одного,</h3>
+                    <p>який за рахунок свого досвіду в будівельно-ремонтній ніші повністю виконує весь спектр онлайн-робіт Вашого бізнесу під ключ!</p>
+                </div>
+            </div>
+            <div class="tablet-block">
+                <img src="images/tablet.svg" alt="tablet">
+
+                <div class="text-block">
+                    <p>Утримування власного маркетинг відділу це сотні тисяч гривень витрат.</p>
+                    <p>Найм багатьох різних агенств на кожнен напрямок - це некоординація та зайві витрати.</p>
+                    <p>Для чого? Хіба не простіше довіритися одній компанії, яка зробить все?</p>
+
+                    <button>
+                        Детальніше
+                        <img src="style/icons/arrow.svg" alt="arrow">
+                    </button>
+
+                </div>
+            </div>
+
+            <div class="blocks-square">
+                <div class="text-block">
+                    <div class="green-block"></div>
+                    <div class="inner-block">
+                        <h3>Ми компанія, яка бере всю відповідальність на себе та знімає всі клопоти з власника бізнесу.</h3>
+                        <h3 class="green">Ми залучаємо до Вашого проекту велику та злагоджену команду та можемо Вам зробити:</h3>
+                    </div>
+                </div>
+                <div class="block">
+                    <div class="top-block">
+                        <p>реклама пошукова</p>
+                        <h3>Таргетована реклама</h3>
+                    </div>
+                    <hr>
+                    <h2>FB&Instagram</h2>
+                </div>
+                <div class="block">
+                    <div class="top-block">
+                        <p>реклама пошукова</p>
+                        <h3>Реклама на маркетплейсах</h3>
+                    </div>
+                    <hr>
+                    <h2>OLX, PROM, HOTLINE</h2>
+                </div>
+                <div class="block">
+                    <div class="top-block">
+                        <p>сайт під ключ</p>
+                        <h3>Унікальний сайт</h3>
+                    </div>
+                    <hr>
+                    <h2>Окрема послуга</h2>
+                </div>
+                <div class="block">
+                    <div class="top-block">
+                        <p>навчання</p>
+                        <h3>Професійне Навчання менеджерів</h3>
+                    </div>
+                    <hr>
+                    <h2>Збільшення конверсії продажів</h2>
+                </div>
+                <div class="block">
+                    <div class="top-block">
+                        <p>реклама пошукова</p>
+                        <h3>Вcі види і типи реклами</h3>
+                    </div>
+                    <hr>
+                    <h2>Google/YouTube</h2>
+                </div>
+                <div class="block">
+                    <div class="top-block">
+                        <p>реклама пошукова</p>
+                        <h3>Повноцінний SMM</h3>
+                    </div>
+                    <hr>
+                    <h2>Той яким він має бути, а не просто “заради картинки”</h2>
+                </div>
+                <div class="block">
+                    <div class="top-block">
+                        <p>програмне забезпечення</p>
+                        <h3>Розробка  BAF</h3>
+                    </div>
+                    <hr>
+                    <h2>Система роботи компанії</h2>
+                </div>
+                <div class="block">
+                    <div class="top-block">
+                        <p>аналітика</p>
+                        <h3>Аналітика компанії</h3>
+                    </div>
+                    <hr>
+                    <h2>Повна та прозора аналітика компанії в діяльності онлайн</h2>
+                </div>
+                <div class="block">
+                    <div class="top-block">
+                        <p>скрипти</p>
+                        <h3>Розробка скриптів під вашу компанію</h3>
+                    </div>
+                    <hr>
+                    <h2>Розробка скриптів для відділу продажів </h2>
+                </div>
+                <div class="block">
+                    <div class="top-block">
+                        <p>менеджмент</p>
+                        <h3>Розробка CRM</h3>
+                    </div>
+                    <hr>
+                    <h2>Інтегрована або стороння</h2>
                 </div>
             </div>
 
--- js/main.js
+++ js/main.js
@@ -13,33 +13,65 @@ activeCheckbox.addEventListener('change', function() {
 
 // script to create BLUR elements
 const container = document.querySelector('.body-content');
-const numberOfDivs = 8; // Set the number of divs you want to create
+let numberOfDivs = 8; // Set the number of divs you want to create (for desktop)
 let topPosition = 200;
 
-for (let i = 0; i < numberOfDivs; i++) {
-    const div = document.createElement("div");
-    div.classList.add("blur");
+if (window.innerWidth >= 1200) {
+    console.log("desktop");
 
-    div.style.top = `${topPosition}px`;
+    for (let i = 0; i < numberOfDivs; i++) {
+        const div = document.createElement("div");
+        div.classList.add("blur");
 
-    if (i % 4 === 0) {
+        div.style.top = `${topPosition}px`;
+
+        if (i % 4 === 0) {
+            topPosition += 500;
+            div.style.left = "20%";
+        } else if (i % 4 === 1) {
+            div.style.left = "70%";
+        } else if (i % 4 === 2) {
+            div.style.left = "30%";
+        } else {
+            div.style.left = "80%";
+        }
+
+        div.style.position = "absolute";
+        div.style.transform = "translate(-50%,-50%)";
+        div.style.width = "296px";
+        div.style.height = "287px";
+        div.style.background = "#05E900";
+        div.style.filter = "blur(250px)";
+
+        container.appendChild(div);
         topPosition += 500;
-        div.style.left = "20%";
-    } else if (i % 4 === 1) {
-        div.style.left = "70%";
-    } else if (i % 4 === 2) {
-        div.style.left = "30%";
-    } else {
-        div.style.left = "80%";
     }
 
-    div.style.position = "absolute";
-    div.style.transform = "translate(-50%,-50%)";
-    div.style.width = "296px";
-    div.style.height = "287px";
-    div.style.background = "#05E900";
-    div.style.filter = "blur(250px)";
+} else {
+    console.log("mobile");
 
-    container.appendChild(div);
-    topPosition += 500;
+    topPosition = 800;
+    numberOfDivs = 4; // Set the number of divs you want to create (for mobile)
+
+    for (let i = 0; i < numberOfDivs; i++) {
+        const div = document.createElement("div");
+        div.classList.add("blur");
+
+        div.style.top = `${topPosition}px`;
+
+        div.style.position = "absolute";
+        div.style.transform = "translate(-50%,-50%)";
+        div.style.left = "50%";
+        div.style.width = "296px";
+        div.style.height = "287px";
+        div.style.background = "#05E900";
+        div.style.filter = "blur(250px)";
+        div.style.opacity = "0.5";
+
+        container.appendChild(div);
+        topPosition += 1500;
+    }
+
 }
+
+
