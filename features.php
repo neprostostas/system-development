2e30be9d6e0808b45f7efd344730c5cd2ac0a707
neprostostas <neprostostas.gg@gmail.com>
2023-03-09 18:06:51 +0200

    swapped script links

--- features.php
+++ features.php
@@ -11,6 +11,8 @@
     <script src="https://kit.fontawesome.com/02e8c33a85.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/imask"></script>
     <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+    <script type="text/javascript" src="js/vanilla-tilt.js"></script>
+    <script src="js/features.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css"/>
     <link rel="stylesheet" href="style/style.css">
 </head>
@@ -402,7 +404,5 @@
 
     <?php require "@commons/footer.php" ?>
 
-    <script src="js/features.js"></script>
-    <script type="text/javascript" src="js/vanilla-tilt.js"></script>
 </body>
 </html>
--- results.php
+++ results.php
@@ -11,6 +11,8 @@
     <script src="https://kit.fontawesome.com/02e8c33a85.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/imask"></script>
     <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+    <script type="text/javascript" src="js/vanilla-tilt.js"></script>
+    <script src="js/results.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css"/>
     <link rel="stylesheet" href="style/style.css">
 </head>
@@ -91,15 +93,11 @@
 
             </div>
 
-            .example
-
 
         </div>
     </div>
 
     <?php require "@commons/footer.php" ?>
 
-    <script src="js/results.js"></script>
-    <script type="text/javascript" src="js/vanilla-tilt.js"></script>
 </body>
 </html>
